fd0d1831783c5efd1b8eaf314c88e7e89c96ad27
Daniel Morales <danieljosem097@gmail.com>
2023-10-03 23:09:58 -0500

    home v1.0.3 carga dinamica cards

--- js/main.js
+++ js/main.js
@@ -7,7 +7,10 @@ const d = document,
   $parentCanvasMobile = d.getElementById("parent-canvas-mobile"),
   $offcanvasMobile = d.querySelector(".offcanvas-mobile");
 
+const $cartBody = d.querySelector(".cart-body");
+
 d.addEventListener("click", (e) => {
+  // Sidebar
   if (e.target.matches(".categories-item")) {
     const $offcanvasBackdrop = d.querySelector(".offcanvas-backdrop");
 
@@ -20,5 +23,68 @@ d.addEventListener("click", (e) => {
       $offcanvasMobile.classList.remove("show");
       $parentCanvasMobile.removeChild($offcanvasBackdrop);
     }
+  }
+
+  // Cart
+  if (e.target.matches("#btn-addCart")) {
+    // e.target.closest(".col").querySelector(".card-body #item-name").textContent;
+    // e.target.closest(".col").querySelector(".card-body #item-price").textContent;
+    // e.target.closest(".col").querySelector(".card-img-top").src;
+    // const $img = d.createElement("img");
+    // $img.src = e.target.closest(".col").querySelector(".card-img-top").src;
+    // d.body.appendChild($img);
+
+    const $itemCart = d.createElement("article"),
+      $img = d.createElement("img"),
+      $itemCartInfo = d.createElement("section"),
+      $itemCartTitle = d.createElement("p"),
+      $itemCartPrice = d.createElement("p"),
+      $itemCartActions = d.createElement("section"),
+      $btnadd = d.createElement("button"),
+      $numItems = d.createElement("span"),
+      $btndecrease = d.createElement("button");
+
+    $itemCart.classList.add("item-cart");
+
+    $img.classList.add("item-cart-img");
+    $img.src = e.target.closest(".col").querySelector(".card-img-top").src;
+    $img.alt = e.target
+      .closest(".col")
+      .querySelector(".card-body #item-name").textContent;
+
+    $itemCartInfo.classList.add("item-cart-info");
+
+    $itemCartTitle.classList.add("item-cart-title");
+    $itemCartTitle.textContent = e.target
+      .closest(".col")
+      .querySelector(".card-body #item-name").textContent;
+
+    $itemCartPrice.classList.add("item-cart-price");
+    $itemCartPrice.textContent = e.target
+      .closest(".col")
+      .querySelector(".card-body #item-price").textContent;
+
+    $itemCartActions.classList.add("item-cart-actions");
+
+    $btnadd.classList.add("btn", "btn-danger");
+    $btnadd.textContent = "+";
+    $btndecrease.classList.add("btn", "btn-success");
+    $btndecrease.textContent = "-";
+
+    $numItems.classList.add("m-2");
+    $numItems.textContent = "1";
+
+    $itemCartActions.appendChild($btnadd);
+    $itemCartActions.appendChild($numItems);
+    $itemCartActions.appendChild($btndecrease);
+
+    $itemCartInfo.appendChild($itemCartTitle);
+    $itemCartInfo.appendChild($itemCartPrice);
+
+    $itemCart.appendChild($img);
+    $itemCart.appendChild($itemCartInfo);
+    $itemCart.appendChild($itemCartActions);
+
+    $cartBody.appendChild($itemCart);
   }
 });
